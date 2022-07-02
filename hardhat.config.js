ed9f684131ac6bb037454a0d2341f3f3f3b22a7e
zak-zaffar <zakzaffar@gmail.com>
2022-07-03 00:35:35 +0100

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,45 @@
+require("@nomiclabs/hardhat-waffle")
+require("dotenv").config()
+require("./tasks/block-number")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://eth-rinkeby"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            chainId: 31337,
+        },
+    },
+    solidity: "0.8.7",
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY, //apikey is written with camelCase K
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "MATIC",
+    },
+}
---
+++ scripts/deploy.js
@@ -0,0 +1,49 @@
+const { ethers, run, network } = require("hardhat")
+
+async function main() {
+    const SimpleStorageFactory = await ethers.getContractFactory(
+        "SimpleStorage"
+    )
+    console.log("deploying contract ...")
+    const simpleStorage = await SimpleStorageFactory.deploy()
+    await simpleStorage.deployed()
+    console.log(`Deployed contract to: ${simpleStorage.address}`)
+    if (network.config.chainId == 4 && process.env.ETHERSCAN_API_KEY) {
+        console.log("Waiting for block confirmations...")
+        await simpleStorage.deployTransaction.wait(6)
+        await verify(simpleStorage.address, [])
+    }
+
+    const currentFavoriteNumber = await simpleStorage.retrieve()
+    console.log(`Current Favorite Number: ${currentFavoriteNumber}`)
+    console.log("Updating favorite number...")
+    const transactionResponse = await simpleStorage.store("7", {
+        gasLimit: 100000,
+    })
+    await transactionResponse.wait(1)
+    const updatedFavoriteNumber = await simpleStorage.retrieve()
+    console.log(updatedFavoriteNumber.toString())
+}
+
+const verify = async (contractAddress, args) => {
+    console.log("Verifying contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified!")
+        } else {
+            console.log(e)
+        }
+    }
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
